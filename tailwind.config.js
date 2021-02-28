bc3605d3228de302ec5db7e8eda3f25a3dd30fc1
Watcharaphon Khanto <macrorobb@outlook.com>
2021-02-28 15:01:26 +0700

    tailwind stories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -8,10 +10,11 @@ module.exports = {
           light: "#3fbaeb",
           DEFAULT: "#0fa9e6",
           dark: "#0c87b8"
-        }
+        },
         // brand: "#0fa9e6",
         // "brand-light": "#3fbaeb",
         // "brand-dark": "#0c87b8"
+        fuchsia: colors.fuchsia,
       },
       fontFamily: {
         headlind: "Poppins, sand-serif" // font-headline
